580c606411631bcb2cd5eb460b3bedfd56f73eca
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2020-01-06 22:30:42 +0300

    Get chain information

--- chains.go
+++ chains.go
@@ -1,3 +1,44 @@
 package ost
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/go-querystring/query"
+)
+
 type ChainsService service
+
+type Chain struct {
+	ID               int    `json:"id,omitempty"`
+	Type             string `json:"type,omitempty"`
+	BlockHeight      int    `json:"block_height,omitempty"`
+	BlockTime        int    `json:"block_time,omitempty"`
+	UpdatedTimestamp int    `json:"updted_timestamp,omitempty"`
+}
+
+type ChainResponse struct {
+	Success bool `json:"success,omitempty"`
+	Data    *struct {
+		ResultType string `json:"result_type,omitempty"`
+		Chain      *Chain `json:"chain,omitempty"`
+	} `json:"data,omitempty"`
+}
+
+func (s *ChainsService) Get(ID int) (*ChainResponse, error) {
+	var err error
+	timestamp := time.Now().Unix()
+	params := QueryParams{
+		ApiKey:              s.client.options.ApiKey,
+		ApiRequestTimestamp: timestamp,
+		ApiSignatureKind:    SIGNATURE_KIND,
+	}
+	v, _ := query.Values(params)
+	resource := fmt.Sprintf("/chains/%d?%s", ID, v.Encode())
+	signature := SignQueryParams(resource, s.client.options.ApiSecret)
+	res := new(ChainResponse)
+	params.ApiSignature = signature
+	price_points := fmt.Sprintf("chains/%d", ID)
+	s.client.base.Get(price_points).QueryStruct(params).Receive(res, err)
+	return res, err
+}
---
+++ example/chains/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/tsirysndr/ost-sdk-go"
+)
+
+func main() {
+	options := ost.Options{
+		Endpoint:  "<API_ENDPOINT>",
+		ApiKey:    "<YOUR_API_KEY>",
+		ApiSecret: "<YOUR_API_SECRET>",
+	}
+	client := ost.NewClient(options)
+	r, _ := client.Chains.Get(3)
+	response, _ := json.Marshal(r)
+	fmt.Println(string(response))
+}
